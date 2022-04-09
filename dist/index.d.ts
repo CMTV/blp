ab634005645cfcc6d34fa5e325f77b21a2f3454c
CMTV <cmtvpetya@gmail.com>
2022-04-09 11:35:28 +0300

    Remove dist

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Product from "./Product";
-import IFactory from "./IFactory";
-import Parser from "./Parser";
-import Range from "./Range";
-import util from "./util";
-import { Block, BlockFactory, BlockObjFactory, Paragraph } from "./block";
-import { Inliner, InlinerFactory, InlinerRegExpFactory, Plain } from "./inliner";
-export { Product, IFactory, Parser, Range, util, Block, BlockFactory, BlockObjFactory, Paragraph, Inliner, InlinerFactory, InlinerRegExpFactory, Plain };
